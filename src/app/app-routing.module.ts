1ad7f27d020891bd62869855c43c42cba48b20c0
s448 <elsaedmohassan@gmail.com>
2023-08-21 21:03:32 +0300

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'recipes', component: RecipesComponent, children: [
+    {path: '', component: RecipeStartComponent},
+    { path: ':id', component: RecipeDetailsComponent}
+  ] },
+  { path: 'shopping-list', component: ShoppingListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
